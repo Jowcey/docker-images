36032960877a6239213ef330ee0ebe5c371bd227
Jowcey <master_adam_maxwell@hotmail.com>
2021-05-09 18:08:53 +0100

    M1DN1GHT Update

--- xvfb-java/entrypoint.sh
+++ xvfb-java/entrypoint.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 cd /home/container
 
-# Output Current Java Version
-java -version ## only really needed to show what version is being used. Should be changed for different applications
+# Make internal Docker IP address available to processes.
+export INTERNAL_IP=ip route get 1 | awk '{print $NF;exit}'
 
 # Replace Startup Variables
-MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-echo ":/home/container$ ${MODIFIED_STARTUP}"
+MODIFIED_STARTUP=$(echo $(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'))
+START_COMMAND=$(echo -e ${MODIFIED_STARTUP})
+echo -e ":/home/container$ ${START_COMMAND}"
 
 # Run the Server
 ${MODIFIED_STARTUP}
